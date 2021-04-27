6027354174bb4ad3bcf2203ca1df2530248d1e7f
Faizan-Zia <faizanzia005@gmail.com>
2021-04-27 19:10:45 +0000

    Push Modules

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard.component';
+import { CalendarModule } from 'angular-calendar';
+
+@NgModule({
+  declarations: [DashboardComponent],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    CalendarModule.forRoot()
+  ]
+})
+export class DashboardModule { }
